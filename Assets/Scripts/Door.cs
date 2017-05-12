93d370efee149448173be24b2ff7e6ffc056233b
khooroko <khoowp25@hotmail.com>
2017-05-12 13:46:22 +0800

    working jump, working door
    need to add the scenes into the build settings before you can load that
    scene from a door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+
+    public string sceneName;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player") {
+            SceneManager.LoadScene(sceneName);           
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,93 +7,42 @@ public class Boundary {
     public float xMin, xMax, yMin, yMax;
 }
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : PhysicsObject {
 
-    private Rigidbody2D rb;
-    public float speed;
-    public Boundary boundary;
-    //public float jumpHeight;
-    public float maxJumpHeight = 4;
-    public float minJumpHeight = 1;
-    public float timeToJumpApex = .4f;
-    float accelerationTimeAirborne = .2f;
-    float accelerationTimeGrounded = .1f;
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 20;
 
-    float gravity;
-    float maxJumpVelocity;
-    float minJumpVelocity;
-    Vector2 velocity;
-    float velocityXSmoothing;
+    private SpriteRenderer spriteRenderer;
+    //private Animator animator;
 
-
-    //private bool jumping;
-    /*private AudioSource audiosource;
-    private GameController gameController;
-    
+    // Use this for initialization
     void Awake() {
-        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
-        //gameController = new GameController();
-        if (gameController == null) {
-            Debug.Log("canot find gamecontroller script");
-        }
-    }
-    */
-    void Start() {
-        rb = GetComponent<Rigidbody2D>();
-        // audiosource = GetComponent<AudioSource>(); 
-        gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
-        maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
-        print("Gravity: " + gravity + "  Jump Velocity: " + maxJumpVelocity);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //animator = GetComponent<Animator>();
     }
 
-    /*void Update() {
-        if((Input.GetButton("Fire1") || Input.GetKey(KeyCode.Space)) && Time.time > nextFire) {
-            nextFire = Time.time + fireRate;
-            Instantiate(shot, shotSpawn[0].position, shotSpawn[0].rotation);
-            if(gameController.getScore() >= 500) {
-                Quaternion shot1Rotation = Quaternion.Euler(0.0f, shotSpawn[1].rotation.y, -rb.rotation.z);
-                Quaternion shot2Rotation = Quaternion.Euler(0.0f, shotSpawn[2].rotation.y, -rb.rotation.z);
-                Instantiate(shot, shotSpawn[1].position, shot1Rotation);
-                Instantiate(shot, shotSpawn[2].position, shot2Rotation);
-            }
-            if(gameController.getScore() >= 2000) {
-                fireRate = 0.0f;
-                gameController.hazardSpawnWait = 0.2f;
-                gameController.enemySpawnWait = 0.4f;
-            }
-            audiosource.Play();
-        } 
-    }*/
+    protected override void ComputeVelocity() {
+        Vector2 move = Vector2.zero;
 
-    void Update() {
-        /*if (rb.velocity.y == 0f && !jumping && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))) {
+        move.x = Input.GetAxis("Horizontal");
 
-        }*/
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            velocity.y = maxJumpVelocity;
+        if (Input.GetButtonDown("Jump") && grounded) {
+            velocity.y = jumpTakeOffSpeed;
         }
-        if (Input.GetKeyUp(KeyCode.Space)) {
-            if (velocity.y > minJumpVelocity) {
-                velocity.y = minJumpVelocity;
+        else if (Input.GetButtonUp("Jump")) {
+            if (velocity.y > 0) {
+                velocity.y = velocity.y * 0.5f;
             }
         }
 
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        if (flipSprite) {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
 
-        velocity.y += gravity * Time.deltaTime;
-        //controller.Move(velocity * Time.deltaTime, input);
-    }
+        //animator.SetBool("grounded", grounded);
+        //animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
 
-        void FixedUpdate () {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        //Vector2 movement = new Vector2(moveHorizontal, 0f);
-        rb.velocity = new Vector2(moveHorizontal * speed, 0);
-        
-        rb.position = new Vector2(Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),  
-                                    Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax ));
-
-        //rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+        targetVelocity = move * maxSpeed;
     }
 }
