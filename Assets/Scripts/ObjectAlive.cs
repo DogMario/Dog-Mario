510fa28fa4c2f631007d04cd95d7d92f4a272037
xxtasy <xxtasy.tbh@gmail.com>
2017-05-13 16:24:55 +0800

    death, marioblock, spikes

---
+++ Assets/Scripts/ObjectAlive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectAlive : MonoBehaviour {
+	//true = alive, false = dead
+	public bool alive = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (alive == false) {
+			Destroy (gameObject);
+			print ("test");
+		}
+	}
+
+	public void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Spikes")
+			alive = false;
+	}
+}
