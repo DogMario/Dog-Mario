9e535759c32e3d31e691c6001a3d27689d753f3c
khooroko <khoowp25@hotmail.com>
2017-05-17 01:37:23 +0800

    Started Level 2

---
+++ Assets/Scripts/Level 2/BGScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour {
+
+    public float scrollSpeed;
+    public float tileX;
+
+    private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+        startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileX);
+        transform.position = startPosition + Vector3.left * newPosition;
+	}
+}
