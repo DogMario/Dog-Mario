15fae974829ae27f91935ce21713770fb0aff769
xxtasy <xxtasy.tbh@gmail.com>
2017-05-22 21:32:58 +0800

    updated level 1, added brick, moveright script

---
+++ Assets/SimpleMoveRight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Simple script to move right
+// for: projectiles, normal enemies
+public class SimpleMoveRight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (transform.right * Time.deltaTime);
+	}
+}
