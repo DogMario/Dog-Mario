e931823a2ad2e30fd325771e1bd268e5381c481b
xxtasy <xxtasy.tbh@gmail.com>
2017-05-23 23:40:20 +0800

    added background, changed the part before flag abit. Added a normal cloud and evil cloud (trap), incomplete as of now, completing that trap tmr

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -7,7 +7,9 @@ public class KillPlayer : MonoBehaviour {
 	//Kills player if collision occurs
 	void OnTriggerEnter2D(Collider2D other) {
 		if (tag == "KillPlayer" && (other.tag == "Player" || other.tag == "DogHead")) {
+
 			other.GetComponentInParent<PlayerController>().Die();
 		}
+
 	}
 }
