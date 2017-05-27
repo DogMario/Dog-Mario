148ee1703204cf97d6aa85576806f8dd2417da61
khooroko <khoowp25@hotmail.com>
2017-05-27 14:08:59 +0800

    (Lv1) added 2 flags maybe we'll remove one later

---
+++ Assets/Scripts/RepawnByCheckpoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepawnByCheckpoint : MonoBehaviour {
+
+    public GameObject[] checkpoints;
+    //an array of gameobjects, drag every flag into this script componenet
+    
+	// Use this for initialization
+	void Start () {
+        if (StaticCheckpoint.checkpoint > 0) {  //if a checkpoint has been passed, the position of the object that this script is attached to (i.e. doggo)
+                                                //will be at the place of the passed checkpoint
+            transform.position = checkpoints[StaticCheckpoint.checkpoint - 1].transform.position;
+        }
+	}
+}
