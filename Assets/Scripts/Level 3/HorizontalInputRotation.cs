85fbe2fdf6e4515a7cd8cda988a97f5af10be65c
khooroko <khoowp25@hotmail.com>
2017-06-03 10:09:55 +0800

    started lv 3, worked the camera

---
+++ Assets/Scripts/Level 3/HorizontalInputRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalInputRotation : MonoBehaviour {
+
+    public float turningSpeed = 45;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
+transform.Rotate(0, horizontal, 0);
+    }
+}
---
+++ Assets/Scripts/Level 3/Level3PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level3PlayerController : MonoBehaviour {
+    public float movementSpeed = 10;
+    public float turningSpeed = 60;
+    private Rigidbody rb;
+    private float lastY;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        rb.AddForce(Camera.main.transform.forward * vertical);
+        float horizontal = Input.GetAxis("Horizontal") * movementSpeed * 0.5f * Time.deltaTime;
+        rb.AddForce(Camera.main.transform.right * horizontal);
+    }
+}
