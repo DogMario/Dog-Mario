4ba95f6e4e44605bd76dd727f255db67ed14d5e2
khooroko <khoowp25@hotmail.com>
2017-07-18 22:06:37 +0800

    (Lv2) hp bar for boss done

---
+++ Assets/Scripts/Level 2/FidgetBossHPBarColour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FidgetBossHPBarColour : MonoBehaviour {
+
+    private FidgetBossMainScript fbMain;
+    private Image image;
+    private bool isRed;
+
+	// Use this for initialization
+	void Start () {
+        fbMain = GetComponentInParent<FidgetBossMainScript>();
+        image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(fbMain.hp <= fbMain.threshold && !isRed) {
+            var red = new Color32(255, 0, 0, 150);
+            image.color = red;
+            isRed = true;
+        }
+	}
+}
---
+++ Assets/Scripts/Level 2/FidgetBossHPBarSlider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FidgetBossHPBarSlider : MonoBehaviour {
+
+    private Slider slider;
+    private FidgetBossMainScript fbMain;
+
+	// Use this for initialization
+	void Start () {
+        slider = GetComponent<Slider>();
+        fbMain = GetComponentInParent<FidgetBossMainScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        slider.value = fbMain.hp;
+	}
+}
